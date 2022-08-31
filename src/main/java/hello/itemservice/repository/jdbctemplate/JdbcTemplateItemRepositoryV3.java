a21405bd34893f4f340a947dc21b126fff4f0a50
jmr <jmr10200@gmail.com>
2022-08-31 21:22:10 +0900

    JdbcTemplate 기능 정리 - comment 추가

--- src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV3.java
+++ src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV3.java
@@ -112,3 +112,55 @@ public class JdbcTemplateItemRepositoryV3 implements ItemRepository {
         return template.query(sql, param, itemRowMapper());
     }
 }
+
+/* JdbcTemplate 기능 정리 */
+// JdbcTemplate : 순서 기반 파라미터 바인딩 지원
+// NamedParameterJdbcTemplate : 이름 기반 파라미터 바인딩 (권장)
+// SimpleJdbcInsert : Insert SQL 편리하게 사용
+// SimpleJdbcCall : Stored Procedure 를 편리하게 호출
+
+// 단건조회
+// int cnt = jdbcTemplate.queryForObject("select count(*) from table_name", Integer.class) : 숫자 조회
+// int cnt = jdbcTemplate.queryForObject("select count(*) from table_name where name=?", Integer.class, "param") : 숫자 조회, 파라미터 바인딩
+// String name = jdbcTemplate.queryForObject("select name from table_name where id=?", String.class, 1001L) : 문자 조회
+// Member member = jdbcTemplate.queryForObject("select name, age from table_name where id=?",
+//             (resultSet, rowNum) -> {
+//                          Member mem = new Member();
+//                          mem.setName(resultSet.getString("name"));
+//                          mem.setAge(resultSet.getInt("age"));
+//                          return mem;
+//              }, 1001L) : 객체 조회
+
+// 목록 조회
+// List<Member> members = jdbcTemplate.query("select name, age from table_name",
+//             (resultSet, rowNum) -> {
+//                          Member mem = new Member();
+//                          mem.setName(resultSet.getString("name"));
+//                          mem.setAge(resultSet.getInt("age"));
+//                          return mem;
+//              }, 1001L) : 객체 -> RowMapper 를 사용해야한다 (람다식)
+
+// RowMapper 를 사용한 객체 목록 조회
+// private final RowMapper<Actor> actorRowMapper = (resultSet, rowNum) -> {
+//     Actor actor = new Actor();
+//     actor.setFirstName(resultSet.getString("first_name"));
+//     actor.setLastName(resultSet.getString("last_name"));
+//     return actor;
+// };
+// public List<Actor> findAllActors() {
+//   return this.jdbcTemplate.query("select first_name, last_name from t_actor", actorRowMapper);
+
+// 등록
+// jdbcTemplate.update("insert into table_name (name, age) values (?, ?)", "billy elliot", "13");
+
+// 수정
+// jdbcTemplate.update("update table_name set name = ? where id = ?", "matilda", 3242L);
+
+// 삭제
+// jdbcTemplate.update("delete from table_name where id = ?", Long.valueOf(memberId));
+
+// DDL
+// jdbcTemplate.execute("create table table_name (id integer, name varchar(100))");
+
+// Stored Procedure
+// jdbcTemplate.update("call SUPPORT.REFRESH_ACTORS_SUMMARY(?)", Long.valueOf(unionId))
